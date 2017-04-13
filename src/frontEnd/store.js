00b69e5173bd2c10b4f3df688f2bc792824eaad5
Shahenaz Monia <shahy.m.93@gmail.com>
2017-04-13 09:58:07 +0300

    Delete store.js

--- src/frontEnd/store.js
+++
@@ -1,9 +0,0 @@
-import {createStore,combineReducers} from 'redux';
-import resourcesReducer from './reduxes.js';
-
-const allReducers = combineReducers({
-    resources: resourcesReducer
-});
-const store = createStore(allReducers);
-
-export default store;
